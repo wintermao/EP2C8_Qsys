45bb6f96ddf95b53a42df2e70f080c3148ba95c7
wintermao <winter_mao@163.com>
2018-06-26 08:14:44 +0800

    修改流模式测试模块 1 去除状态机 2 发送模块增加无效数据状态的输出 3 接收模块增加等待输出 4 增加signaltap测试 结果与预期不符，数据跳跃，间隔4byte，待分析

--- ip/sink_component/hdl/sink_component.v
+++ ip/sink_component/hdl/sink_component.v
@@ -9,62 +9,53 @@
 // to your version control system if you want to keep it.
 
 `timescale 1 ps / 1 ps
-`define ACCESS_ADDR 32'h1001000
+//`define ACCESS_ADDR 32'h1001000
 
 module sink_component (
-		input  wire [31:0] asi_in0_data,  //   in0.data
+		input  wire [15:0] asi_in0_data,  //   in0.data
 		output reg        asi_in0_ready, //      .ready
 		input  wire        asi_in0_valid, //      .valid
 		input  wire        clk,           // clock.clk
 		input  wire        reset,          // reset.reset
-		
+		output reg [15:0] data
 	// write master port interface
-		output	reg	[31:0]	avm_write_address,
-		output	reg	avm_write_write,
-		output	reg	[31:0]	avm_write_writedata,
-		input	 wire avm_write_waitrequest
+//		output	reg	[31:0]	avm_write_address,
+//		output	reg	avm_write_write,
+//		output	reg	[31:0]	avm_write_writedata,
+//		input	 wire avm_write_waitrequest
 	);
 
 reg 	[31:0]	DMA_Cont;
 
-reg	[5:0]	DMA_state;
-parameter	DMA_IDLE				=	0;
-parameter	READ					=	1;
-parameter	WAIT_READ				=	2;
-parameter	WRITE				=	3;
-parameter	WAIT_WRITE			=	4;
-parameter	CALC_NEXT				=	5;
-parameter	DMA_DONE				=	6;
+//reg	[5:0]	DMA_state;
+//parameter	DMA_IDLE				=	0;
+//parameter	READ					=	1;
+//parameter	WAIT_READ				=	2;
+//parameter	WRITE				=	3;
+//parameter	WAIT_WRITE			=	4;
+//parameter	CALC_NEXT				=	5;
+//parameter	DMA_DONE				=	6;
 
 always@(posedge clk or posedge reset)
 begin
 	if(reset) begin
-		DMA_Cont 	<= 32'h0;
-		avm_write_address <= `ACCESS_ADDR;
-		DMA_state <= WRITE;
+		DMA_Cont 	<= 16'h0;
+		data <= 16'h0;
+		asi_in0_ready<=1'b1;
 	end	else begin
-		case(DMA_state)
-			WRITE: begin
-				avm_write_address <= `ACCESS_ADDR + DMA_Cont;
-				if(asi_in0_valid==1'b1) begin
-					avm_write_write <= 1'b1;
-					avm_write_writedata <= asi_in0_data;
-					DMA_state <= WAIT_WRITE;
-				end else begin
-					avm_write_write <= avm_write_write;
-					DMA_state <= DMA_state;
-				end
+		if(asi_in0_valid==1'b1) begin
+			DMA_Cont <= DMA_Cont + 1'b1;
+			if(DMA_Cont>16'd10 && DMA_Cont<16'd15) begin
+				asi_in0_ready<=1'b0;
+			end else begin 
+				asi_in0_ready<=1'b1;
+				data <= asi_in0_data;
 			end
-			WAIT_WRITE: begin
-				if(avm_write_waitrequest == 1'b0 ) begin
-					avm_write_write <= 1'b0;
-					if(DMA_Cont<32'd64)	DMA_Cont <= DMA_Cont + 32'h4;
-					else DMA_Cont <= 32'd0;
-					DMA_state <= WRITE;
-					asi_in0_ready <= 1'b1;
-				end	else asi_in0_ready <= 1'b0;
-			end
-		endcase
+		end else begin
+			data <= data;
+			asi_in0_ready <= asi_in0_ready;
+			DMA_Cont <= DMA_Cont;
+		end
 	end
 end
 	// TODO: Auto-generated HDL template
--- ip/source_component/hdl/source_component.v
+++ ip/source_component/hdl/source_component.v
@@ -9,61 +9,57 @@
 // to your version control system if you want to keep it.
 
 `timescale 1 ps / 1 ps
-`define ACCESS_ADDR 32'h900000
+//`define ACCESS_ADDR 32'h900000
 
 module source_component (
-		output reg [31:0] aso_out0_data,  //  out0.data
+		output reg [15:0] aso_out0_data,  //  out0.data
 		input  wire        aso_out0_ready, //      .ready
 		output reg        aso_out0_valid, //      .valid
 		input  wire        clk,            // clock.clk
-		input  wire        reset,           // reset.reset
+		input  wire        reset           // reset.reset
 		// read master port interface
-		output reg [31:0] avm_read_address,
-		output reg 				avm_read_read,
-		input  wire [31:0] avm_read_readdata,
-		input	 wire				avm_read_waitrequest
+		//output reg [31:0] avm_read_address,
+		//output reg 				avm_read_read,
+		//input  wire [31:0] avm_read_readdata,
+		//input	 wire				avm_read_waitrequest
 	);
 	
-reg 	[31:0]	DMA_Cont;
+reg 	[15:0]	DMA_Cont;
 
-reg	[5:0]	DMA_state;
-parameter	DMA_IDLE				=	0;
-parameter	READ					=	1;
-parameter	WAIT_READ				=	2;
-parameter	WRITE				=	3;
-parameter	WAIT_WRITE			=	4;
-parameter	CALC_NEXT				=	5;
-parameter	DMA_DONE				=	6;
+//reg	[5:0]	DMA_state;
+//parameter	DMA_IDLE				=	0;
+//parameter	READ					=	1;
+//parameter	WAIT_READ				=	2;
+//parameter	WRITE				=	3;
+//parameter	WAIT_WRITE			=	4;
+//parameter	CALC_NEXT				=	5;
+//parameter	DMA_DONE				=	6;
+reg [15:0] data;
 
 always@(posedge clk or posedge reset)
 begin
 	if(reset) begin
-		DMA_Cont 	<= 32'h0;
-		avm_read_address <= `ACCESS_ADDR;
-		DMA_state <= READ;
+		DMA_Cont 	<= 16'h0;
+		data <= 16'h0;
+		aso_out0_valid<=1'b0;
 	end	else begin
-		case(DMA_state)
-			READ: begin
-				avm_read_address <= `ACCESS_ADDR + DMA_Cont;
-				if(aso_out0_ready==1'b1) begin
-					avm_read_read <= 1'b1;
-					DMA_state	<= WAIT_READ;
-				end else begin
-					avm_read_read <= avm_read_read;
-					DMA_state	<= DMA_state;
-				end
+		if(aso_out0_ready==1'b1) begin
+			aso_out0_data<=data;
+			if(DMA_Cont<16'd32)	begin
+				DMA_Cont <= DMA_Cont + 1'b1;
+				data <= data +1'b1;
+				if(data>16'd16) aso_out0_valid<=0;
+				else aso_out0_valid<=1;
+			end else begin
+				DMA_Cont <= 16'd0;
+				data <= 16'h0;
+				aso_out0_valid<=0;
 			end
-			WAIT_READ: begin
-				if(avm_read_waitrequest == 1'b0 ) begin
-					avm_read_read <= 1'b0;
-					aso_out0_data <= avm_read_readdata;
-					if(DMA_Cont<32'd64)	DMA_Cont <= DMA_Cont + 32'h4;
-					else DMA_Cont <= 32'd0;
-					DMA_state <= READ;
-					aso_out0_valid <= 1'b1;
-				end else aso_out0_valid <= 1'b0;
-			end
-		endcase
+		end else begin
+			DMA_Cont <= DMA_Cont;
+			data <= data;
+			aso_out0_valid <= aso_out0_valid;
+		end
 	end
 end
 
